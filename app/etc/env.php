36b8714ecd034634b05e41ae4dec273924b1950c
chrislpatton <chrislpatton@gmail.com>
2017-01-16 01:15:54 +0000

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 06 May 2016 06:57:56 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '4acdb4d2789ee77b5fcff4ad08c0a41b',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'magento',
+        'username' => 'magento',
+        'password' => 'magento',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
